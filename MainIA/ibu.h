1ce61aa334c8a287e651119a3ac16d387143eb9b
Jantopthegreat <fauzangriya@gmail.com>
2023-05-24 20:39:35 +0700

    membuat class ibu dengan header dan menambahkan member dari ibu

---
+++ MainIA/ibu.h
@@ -0,0 +1,19 @@
+#ifndef IBU_H
+#define IBU_H
+class ibu {
+public:
+	string nama;
+	vector<anak*> daftar_anak;
+
+
+	ibu(string pNama) :nama(pNama) {
+		cout << "ibu \"" << "\" ada\n";
+	}
+	~ibu() {
+		cout << "ibu \"" << nama << "\" tidak ada\n";
+
+	}
+	void tambahAnak(anak* pAnak);
+	void cetakAnak();
+};
+
