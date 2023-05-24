d43226d46dde6dd69327adccd3570a0d59321cce
Jantopthegreat <fauzangriya@gmail.com>
2023-05-24 20:14:37 +0700

    int main pelajar siswa 1

--- ParadigmaOOP/ParadigmaOOP.cpp
+++ ParadigmaOOP/ParadigmaOOP.cpp
@@ -35,3 +35,11 @@ public:
 		return "hallo nama saya " + nama + "dari sekolah " + sekolah + "\n\n";
 	}
 };
+
+
+int main() {
+	pelajar siswa1("andi laksono", "SMA 1 Bantul");
+	cout << siswa1.perkenalan();
+	cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+	return 0;
+}
